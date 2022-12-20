d92b585a5f0e766683a54cd81425c45c6035985e
Arnaud Odet <arnaud.odet@gmail.com>
2022-12-20 10:11:48 +0100

    website test

--- website.py
+++ website.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import requests
+from PIL import Image
 
 API_URL = "https://kyo-va-la-pj6drrveta-od.a.run.app"
 
@@ -8,6 +9,9 @@ quote = requests.get(API_URL,timeout=20).json()
 st.title('Kyo va là !')
 
 st.write(quote['Kyo'])
+
+kyo = Image.open('kyo.jpg')
+st.image(kyo)
 
 st.write(
     'Please refresh to get another random Kyo quote to enlight your day !'
